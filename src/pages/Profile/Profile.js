c902c60df334565baaebaffcb5032cc0e7c23a63
Dixon Wu <dxnwuconnect@gmail.com>
2023-02-19 13:13:05 -0800

    implement profile page

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,10 +1,26 @@
 import "./Profile.scss"
-
+import profileTop from "../../assets/images/profile-top.png"
+import profileBottom from "../../assets/images/profile-bottom.png"
 function Profile () {
 
     return (
         <>
-        
+        <section className="section section--profile-top">
+            <div className="body-wrap">
+                <div className="profile-top">
+                    <img className="profile-top__placeholder" src={profileTop} alt="" />
+
+                </div>
+            </div>
+        </section>
+        <section className="section section--profile-bottom">
+            <div className="body-wrap">
+                <div className="profile-bottom">
+                    <img className="profile-bottom__placeholder" src={profileBottom} alt="" />
+
+                </div>
+            </div>
+        </section>
         </>
     )
 }
